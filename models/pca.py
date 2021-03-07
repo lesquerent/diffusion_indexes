b684c77786702f1c80c04ccc6c36e4c4f7c92ce0
Clément DAVID <clement.david@edu.devinci.fr>
2021-03-07 14:45:12 +0100

    Update of PCA features

--- models/pca.py
+++ models/pca.py
@@ -4,6 +4,8 @@ from sklearn import decomposition
 from numpy import linalg as LA
 
 
+
+
 def select_component(pca_data, percent):
     """
     Returns the number of components such that percent% of the information is explained.
