3476668bae1e3cbada9a35952017be0671703ff4
lesquerent <thibaultls@outlook.fr>
2021-03-16 11:09:37 +0100

    cleaning code

--- data_processing.py
+++ data_processing.py
@@ -25,102 +25,7 @@ def get_tickers_in_dict(excel_file_path, sheet_name_stock):
     return tickers_dict
 
 
-def create_stocks_df_date(my_dict, start_date='2020-01-01', end_date='2021-03-12', data_type='returns', remove_nan_by='row'):
-    """
-
-    Parameters
-    ----------
-    my_dict : dict
-        Dictionary containing key = Stock, value=ticker.
-    start_date: str
-        Corresponding to the beginning of the history.
-        format : AAAA-MM-DD
-        Default : '2020-01-01'
-    end_date: str
-        Corresponding to the end of the history
-        format : AAAA-MM-DD
-        Default : 2021-03-12
-    data_type : str
-        'returns' ou 'prices', cor corresponds to the type of data we want.
-        Default : 'returns'
-    remove_nan_by : str
-        'row' ou 'col' corresponds to the method of removal of NaN.
-        Default : 'row'
-
-    Returns
-    -------
-    dataFrame : pd.DataFrame
-        Corresponds to a dataframe containing prices or returns at the close of the share for
-        all dates of the selected period.
-
-
-    """
-
-    # //////Test on parameters\\\\\\#
-    try:
-        assert data_type == 'prices' or data_type == 'returns'
-    except AssertionError:
-        print("!!ERROR!! create_stocks_df accepts only data_type='prices' or data_type='returns' !!ERROR!!")
-        return
-
-    try:
-        assert remove_nan_by == 'col' or remove_nan_by == 'row'
-    except AssertionError:
-        print("!!ERROR!! create_stocks_df accepts only suppr_nan_by ='col' or suppr_nan_by='row' !!ERROR!!")
-        return
-
-    # //////Data recovery\\\\\\#
-
-    dataFrame = pd.DataFrame()
-
-    # We browse the Tickers dictionary
-    for key, value in my_dict.items():
-        # Price data is retrieved at closing time
-        data = yf.Ticker(value).history(start=start_date, end=end_date)["Close"]
-
-        # If we want to recover the yields (in percentage)
-        if data_type == 'returns':
-            data = yf.Ticker(value).history(start=start_date, end=end_date)["Close"]
-            data = data.pct_change()
-
-        # Adds data in the form of yield or price depending on the chosen option
-        try:
-            dataFrame[key] = data
-        except ValueError:
-            print("INFO : L'action {key} ({value}) ne sera pas dans le data set.".format(key=key, value=value))
-
-    # //////Removal of NaN\\\\\\#
-    # If you want to delete columns that contain NaN
-    if remove_nan_by == 'col':
-        # If we are in the case of yield we must delete the first line because the calculation
-        # of yields induces NaN on the first line.
-        if data_type == 'returns':
-            # Look for lines that have NaN
-            index_with_nan = dataFrame.index[dataFrame.isnull().any(axis=1)].tolist()
-            # Removes the first one induced by the returns of the data frame
-            dataFrame.drop(index_with_nan[0], 0, inplace=True)
-            # Searches for columns that have NaN
-            column_with_nan = dataFrame.columns[dataFrame.isnull().any()].tolist()
-            # Deletes these columns
-            dataFrame.drop(column_with_nan, 1, inplace=True)
-
-        else:  # case data_type=prices
-            # Searches for columns that have NaN
-            column_with_nan = dataFrame.columns[dataFrame.isnull().any()].tolist()
-            # Deletes these columns
-            dataFrame.drop(column_with_nan, 1, inplace=True)
-
-    else:  # case remove_nan_by=='row'
-
-        # Look for lines that have NaN
-        index_with_nan = dataFrame.index[dataFrame.isnull().any(axis=1)].tolist()
-        # Removes the first one induced by the returns of the data frame
-        dataFrame.drop(index_with_nan, 0, inplace=True)
-
-    return dataFrame
-
-
-def create_stocks_df_period(my_dict, period='1mo', data_type='returns', remove_nan_by='row'):
+def create_stocks_df(my_dict, period='1mo', data_type='returns', remove_nan_by='row'):
     """
 
     Parameters
@@ -208,6 +113,7 @@ def create_stocks_df_period(my_dict, period='1mo', data_type='returns', remove_n
         dataFrame.drop(index_with_nan, 0, inplace=True)
 
     return dataFrame
+
 
 if __name__ == '__main__':
     # Path to the excel file containing all the tickers
--- test_files/sandbox.py
+++ test_files/sandbox.py
@@ -1,7 +1,7 @@
 from sklearn import decomposition
 from sklearn.linear_model import LinearRegression
 import annex.constants as const
-from data_processing import create_stocks_df_period
+from data_processing import create_stocks_df
 from models.pca import select_component
 from statsmodels.tsa.api import VAR
 import matplotlib.pyplot as plt
@@ -27,7 +27,7 @@ w
     """
 
     # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    CAC40_Stocks_returns = create_stocks_df_period(dict_of_tickers, history_period)
+    CAC40_Stocks_returns = create_stocks_df(dict_of_tickers, history_period)
 
     # Removal of values from the CAC40 index
     cac40_stocks_returns = CAC40_Stocks_returns.drop(['CAC40'], axis=1)
@@ -56,12 +56,14 @@ w
 
     return array_of_forecast_pc
 
-
 def make_prediction(period):
     forecast_return_ = create_forecast_return_array(const.tickers_CAC40_dict)
+    # print(forecast_return_)
+    #
+    # print(forecast_return_.shape)
 
     # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    CAC40_Stocks_prices = create_stocks_df_period(const.tickers_CAC40_dict, period, data_type='prices')
+    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, period, data_type='prices')
 
     # Removal of values from the CAC40 index
     cac40_stocks_prices = CAC40_Stocks_prices.drop(['CAC40'], axis=1)
@@ -73,7 +75,7 @@ def make_prediction(period):
     # print(cac40_stocks_prices)
 
     # CAC40 index linear reg
-    df_price = create_stocks_df_period(const.tickers_CAC40_dict, period, data_type='prices', remove_nan_by='row')
+    df_price = create_stocks_df(const.tickers_CAC40_dict, period, data_type='prices', remove_nan_by='row')
     df_index = df_price['CAC40']
     df_price = df_price.drop(['CAC40'], axis=1)
     # Model
@@ -87,7 +89,6 @@ def make_prediction(period):
     # plt.show()
     return predictions[-1]
 
-
 def main1():
     forecast_return_ = create_forecast_return_array(const.tickers_CAC40_dict)
     # print(forecast_return_)
@@ -95,15 +96,19 @@ def main1():
     # print(forecast_return_.shape)
 
     # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    CAC40_Stocks_prices = create_stocks_df_period(const.tickers_CAC40_dict, '2y', data_type='prices')
+    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, '2y', data_type='prices')
 
     # Removal of values from the CAC40 index
     cac40_stocks_prices = CAC40_Stocks_prices.drop(['CAC40'], axis=1)
 
     forecast_prices = cac40_stocks_prices * (1 + forecast_return_)
 
+    # print(forecast_prices)
+    #
+    # print(cac40_stocks_prices)
+
     # CAC40 index linear reg
-    df_price = create_stocks_df_period(const.tickers_CAC40_dict, "2y", data_type='prices', remove_nan_by='row')
+    df_price = create_stocks_df(const.tickers_CAC40_dict, "2y", data_type='prices', remove_nan_by='row')
     df_index = df_price['CAC40']
     df_price = df_price.drop(['CAC40'], axis=1)
     # Model
@@ -119,7 +124,7 @@ def main1():
 
 
 def main2():
-    df_price = create_stocks_df_period(const.tickers_CAC40_dict, "6mo", data_type='prices', remove_nan_by='row')
+    df_price = create_stocks_df(const.tickers_CAC40_dict, "6mo", data_type='prices', remove_nan_by='row')
     df_price = df_price.drop(['CAC40'], axis=1)
 
     print(df_price.iloc[df_price.shape[0] - 1, :])
@@ -127,11 +132,13 @@ def main2():
 
 if __name__ == "__main__":
     # main1()
+    df_price = create_stocks_df(const.tickers_CAC40_dict, "6mo", data_type='prices', remove_nan_by='row')
+    print(df_price)
     cac_prediction_6mo = int(round(make_prediction('6mo')))
-    # cac_prediction_2y = int(round(make_prediction('2y')))
-    # cac_prediction_1y = int(round(make_prediction('1y')))
+    #cac_prediction_2y = int(round(make_prediction('2y')))
+    #cac_prediction_1y = int(round(make_prediction('1y')))
 
-    print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 6 derniers mois.'.format(
-        cac_prediction_6mo))
-    # print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 12 derniers mois.'.format(cac_prediction_1y))
-    # print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 24 derniers mois.'.format(cac_prediction_2y))
+    print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 6 derniers mois.'.format(cac_prediction_6mo))
+    #print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 12 derniers mois.'.format(cac_prediction_1y))
+    #print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 24 derniers mois.'.format(cac_prediction_2y))
+
