42e58cb15fb85f1318497a433ac1bd06056879c7
lesquerent <thibaultls@outlook.fr>
2021-03-28 03:17:35 +0200

    Update dict of tickers

--- annex/constants.py
+++ annex/constants.py
@@ -1,25 +1,35 @@
-tickers_CAC40_dict = {'Air Liquide': 'AI.PA', 'Airbus': 'AIR.PA', 'Alstom': 'ALO.PA', 'ArcelorMittal': 'MT.AS',
-                      'Atos': 'ATO.PA', 'AXA': 'CS.PA', 'BNP Paribas': 'BNP.PA', 'Bouygues': 'EN.PA',
-                      'Capgemini': 'CAP.PA', 'Carrefour': 'CA.PA', 'Crédit Agricole': 'ACA.PA', 'Danone': 'BN.PA',
-                      'Dassault Systèmes': 'DSY.PA', 'Engie': 'ENGI.PA', 'EssilorLuxottica': 'EL.PA',
-                      'Hermès': 'RMS.PA', 'Kering': 'KER.PA', "L'Oréal": 'OR.PA', 'Legrand': 'LR.PA',
-                      'LVMH': 'MC.PA', 'Michelin': 'ML.PA', 'Orange': 'ORA.PA', 'Pernod Ricard': 'RI.PA',
-                      'Publicis': 'PUB.PA', 'Renault': 'RNO.PA', 'Safran': 'SAF.PA', 'Saint-Gobain': 'SGO.PA',
-                      'Sanofi': 'SAN.PA', 'Schneider Electric': 'SU.PA', 'Société Générale': 'GLE.PA', 'STMicroelectronics': 'STM.PA', 'Teleperformance': 'TEP.PA',
-                      'Thales': 'HO.PA', 'Total': 'FP.PA', 'Unibail-Rodamco-Westfield': 'URW.AS',
-                      'Veolia': 'VIE.PA', 'Vinci': 'DG.PA', 'Vivendi': 'VIV.PA', 'Worldline': 'WLN.PA',
-                      'CAC40': '^FCHI'}
+tickers_CAC40_dict = {
+						'Accor': 'AC.PA', 'Air Liquide': 'AI.PA',
+						'Airbus': 'AIR.PA', 'ArcelorMittal': 'MT.AS', 'Atos': 'ATO.PA',
+						'AXA': 'CS.PA', 'BNP Paribas': 'BNP.PA', 'Bouygues': 'EN.PA', 
+						'Capgemini': 'CAP.PA', 'Carrefour': 'CA.PA', 'Crédit Agricole': 'ACA.PA', 
+						'Danone': 'BN.PA', 'Dassault Systèmes': 'DSY.PA', 'Engie': 'ENGI.PA',
+						'Essilor': 'EL.PA', 'Hermès': 'RMS.PA', 'Kering': 'KER.PA', 
+						"L'Oréal": 'OR.PA', 'Legrand': 'LR.PA', 'LVMH': 'MC.PA', 'Michelin': 'ML.PA',
+						'Orange': 'ORA.PA', 'Pernod Ricard': 'RI.PA', 'PSA': 'UG.PA',
+						'Publicis': 'PUB.PA', 'Renault': 'RNO.PA', 'Safran': 'SAF.PA', 
+						'Saint-Gobain': 'SGO.PA', 'Sanofi': 'SAN.PA', 'Schneider Electric': 'SU.PA',
+						'Société Générale': 'GLE.PA', 'Sodexo': 'SW.PA', 'STMicroelectronics': 'STM.PA', 
+						'Thales': 'HO.PA', 'Total': 'FP.PA', 'Unibail-Rodamco-Westfield': 'URW.AS',
+						'Veolia': 'VIE.PA', 'Vinci': 'DG.PA', 'Vivendi': 'VIV.PA', 'Worldline': 'WLN.PA',
+						'CAC40': '^FCHI'
+						}
 
 
-tickers_CAC40_dict_2 = {'Air Liquide': 'AI.PA', 'Airbus': 'AIR.PA', 'Alstom': 'ALO.PA', 'ArcelorMittal': 'MT.AS',
-                      'Atos': 'ATO.PA', 'AXA': 'CS.PA', 'BNP Paribas': 'BNP.PA', 'Bouygues': 'EN.PA',
-                      'Capgemini': 'CAP.PA', 'Carrefour': 'CA.PA', 'Crédit Agricole': 'ACA.PA', 'Danone': 'BN.PA',
-                      'Dassault Systèmes': 'DSY.PA', 'Engie': 'ENGI.PA', 'EssilorLuxottica': 'EL.PA',
-                      'Hermès': 'RMS.PA', 'Kering': 'KER.PA', "L'Oréal": 'OR.PA', 'Legrand': 'LR.PA',
-                      'LVMH': 'MC.PA', 'Michelin': 'ML.PA', 'Orange': 'ORA.PA', 'Pernod Ricard': 'RI.PA',
-                      'Publicis': 'PUB.PA', 'Renault': 'RNO.PA', 'Safran': 'SAF.PA', 'Saint-Gobain': 'SGO.PA',
-                      'Sanofi': 'SAN.PA', 'Schneider Electric': 'SU.PA', 'Société Générale': 'GLE.PA',
-                      'Stellantis': 'STLA.PA', 'STMicroelectronics': 'STM.PA', 'Teleperformance': 'TEP.PA',
-                      'Thales': 'HO.PA', 'Total': 'FP.PA', 'Unibail-Rodamco-Westfield': 'URW.AS',
-                      'Veolia': 'VIE.PA', 'Vinci': 'DG.PA', 'Vivendi': 'VIV.PA', 'Worldline': 'WLN.PA',
-                      'CAC40': '^FCHI'}
+tickers_CAC40_dict_2 = {
+						'Air Liquide': 'AI.PA', 'Airbus': 'AIR.PA', 
+						'Alstom': 'ALO.PA', 'ArcelorMittal': 'MT.AS', 'Atos': 'ATO.PA',
+						'AXA': 'CS.PA', 'BNP Paribas': 'BNP.PA', 'Bouygues': 'EN.PA', 
+						'Capgemini': 'CAP.PA', 'Carrefour': 'CA.PA', 'Crédit Agricole': 'ACA.PA',
+						'Danone': 'BN.PA', 'Dassault Systèmes': 'DSY.PA', 'Engie': 'ENGI.PA', 
+						'EssilorLuxottica': 'EL.PA', 'Hermès': 'RMS.PA', 'Kering': 'KER.PA',
+						"L'Oréal": 'OR.PA', 'Legrand': 'LR.PA', 'LVMH': 'MC.PA', 
+						'Michelin': 'ML.PA', 'Orange': 'ORA.PA', 'Pernod Ricard': 'RI.PA',
+						'Publicis': 'PUB.PA', 'Renault': 'RNO.PA', 'Safran': 'SAF.PA', 
+						'Saint-Gobain': 'SGO.PA', 'Sanofi': 'SAN.PA', 'Schneider Electric': 'SU.PA',
+						'Société Générale': 'GLE.PA', 'Stellantis': 'STLA.PA',
+						'STMicroelectronics': 'STM.PA', 'Teleperformance': 'TEP.PA', 'Thales': 'HO.PA', 
+						'Total': 'FP.PA', 'Unibail-Rodamco-Westfield': 'URW.AS', 'Veolia': 'VIE.PA', 
+						'Vinci': 'DG.PA', 'Vivendi': 'VIV.PA', 'Worldline': 'WLN.PA',
+						'CAC40': '^FCHI'
+						}
