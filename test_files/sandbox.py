11d8e9b29312e2cb83f779e5c7207faab56edf94
lesquerent <thibaultls@outlook.fr>
2021-03-28 04:32:33 +0200

    updated prediction function

--- test_files/sandbox.py
+++ test_files/sandbox.py
@@ -3,16 +3,17 @@ from sklearn import decomposition
 from sklearn.linear_model import LinearRegression
 import annex.constants as const
 from data_processing import create_stocks_df
-from models.pca import select_component
 from statsmodels.tsa.api import VAR
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
 
+from ml_models.pca_model import select_component
+
 
 def create_forecast_return_array(dict_of_tickers, history_period='6mo'):
     """
-w
+
     Parameters
     ----------
     dict_of_tickers : dict
@@ -65,7 +66,7 @@ def make_prediction(period):
     forecast_return_ = create_forecast_return_array(const.tickers_CAC40_dict)
 
     # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, period, data_type='prices')
+    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, period)
 
     # Removal of values from the CAC40 index
     cac40_stocks_prices = CAC40_Stocks_prices.drop(['CAC40'], axis=1)
@@ -73,7 +74,7 @@ def make_prediction(period):
     forecast_prices = cac40_stocks_prices * (1 + forecast_return_)
 
     # CAC40 index linear reg
-    df_price = create_stocks_df(const.tickers_CAC40_dict, period, data_type='prices', remove_nan_by='row')
+    df_price = create_stocks_df(const.tickers_CAC40_dict, period)
     df_index = df_price['CAC40']
     df_price = df_price.drop(['CAC40'], axis=1)
     # Model
@@ -95,7 +96,7 @@ def main1():
     # print(forecast_return_.shape)
 
     # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, '2y', data_type='prices')
+    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, '2y')
 
     # Removal of values from the CAC40 index
     cac40_stocks_prices = CAC40_Stocks_prices.drop(['CAC40'], axis=1)
@@ -107,7 +108,7 @@ def main1():
     # print(cac40_stocks_prices)
 
     # CAC40 index linear reg
-    df_price = create_stocks_df(const.tickers_CAC40_dict, "2y", data_type='prices', remove_nan_by='row')
+    df_price = create_stocks_df(const.tickers_CAC40_dict, "2y")
     df_index = df_price['CAC40']
     df_price = df_price.drop(['CAC40'], axis=1)
     # Model
@@ -123,25 +124,29 @@ def main1():
 
 
 def main2():
-    # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    dict_of_tickers = const.tickers_CAC40_dict
-    history_period = '6mo'
+    last_date = '2021-03-29'
+    # Create CAC40 stocks dataFrames
+    tickers_cac40_dict = const.tickers_cac40_dict_2
+    df_prices_returns = create_stocks_df(tickers_cac40_dict, last_date)
+    df_stocks_prices = df_prices_returns[0]
+    df_stocks_returns = df_prices_returns[1]
 
-    CAC40_Stocks_returns = create_stocks_df(dict_of_tickers, history_period)
+    # Create CAC40 index dataFrames
+    df_index_value = df_stocks_prices['^FCHI']
+    df_index_returns = df_stocks_returns['^FCHI']
 
-    # Removal of values from the CAC40 index
-    cac40_stocks_returns = CAC40_Stocks_returns.drop(['CAC40'], axis=1)
-    print(cac40_stocks_returns)
+    del df_stocks_prices['^FCHI']
+    del df_stocks_returns['^FCHI']
 
-    #PCA
-    mu = np.mean(cac40_stocks_returns, axis=0)
+    # PCA
+    mu = np.mean(df_stocks_returns, axis=0)
     pca = sklearn.decomposition.PCA()
-    pca.fit(cac40_stocks_returns)
+    pca.fit(df_stocks_returns)
     nb_component = select_component(pca, 95)
     print(nb_component)
 
     # VAR
-    array_of_principal_components = pca.transform(cac40_stocks_returns)[:, :nb_component]
+    array_of_principal_components = pca.transform(df_stocks_returns)[:, :nb_component]
 
     model = VAR(array_of_principal_components)
 
@@ -151,56 +156,40 @@ def main2():
     lag_order = results.k_ar
     print(lag_order)
 
-    array_of_forecast_pc = results.forecast(array_of_principal_components[-lag_order:], 1)
-
-    results.plot_forecast(15)
-    plt.show()
+    array_of_forecast_pc = results.forecast(array_of_principal_components[-lag_order:], 5)
+    print('Array of forcast pc shape : {}'.format(array_of_forecast_pc.shape))
+    # results.plot_forecast(15)
+    # plt.show()
 
     Xhat = np.dot(array_of_forecast_pc, pca.components_[:nb_component, :])
     Xhat += mu
-    print(Xhat)
+    # Xhat = np.cumprod(1+Xhat,axis=0)
+    print('X hat shape : {}'.format(Xhat.shape))
+    print('X hat : {}'.format(Xhat))
     # forecast_return = pca.inverse_transform(array_of_forecast_pc)
 
     # print(forecast_return)
-
-    # DataFrame containing CAC40 stock returns, with NaN suppression per line
-    CAC40_Stocks_prices = create_stocks_df(const.tickers_CAC40_dict, history_period, data_type='prices')
-
-    # Removal of values from the CAC40 index
-    cac40_stocks_prices = CAC40_Stocks_prices.drop(['CAC40'], axis=1)
-
-    forecast_prices = cac40_stocks_prices * (1 + Xhat)
-
-    # CAC40 index linear reg
-    df_price = create_stocks_df(const.tickers_CAC40_dict, history_period, data_type='prices', remove_nan_by='row')
-    df_index = df_price['CAC40']
-    df_price = df_price.drop(['CAC40'], axis=1)
+    print(df_stocks_prices.loc['2021-03-26', :].shape)
+    price_today = df_stocks_prices.loc['2021-03-26', :].values.reshape(1, -1)
+    print('price today shape : {}'.format(price_today.shape))
+    # forecast_prices = df_stocks_prices.loc['2021-03-26', :] * (1 + Xhat)
+    forecast_prices = price_today * (1 + Xhat)
+    # forecast_prices = price_today * Xhat
+    print('Known prices shape : {}'.format(df_stocks_prices.shape))
+    print('Forecast prices shape : {}'.format(forecast_prices.shape))
     # Model
 
     model = LinearRegression()
-    model.fit(df_price, df_index)
-    model.score(df_price, df_index)
+    model.fit(df_stocks_prices, df_index_value)
+    model.score(df_stocks_prices, df_index_value)
 
     predictions = model.predict(forecast_prices)
     # plt.plot(df_index.index, predictions)
     # plt.show()
 
-    return predictions[-1]
+    return predictions
 
 
 if __name__ == "__main__":
-    # # main1()
-    # df_prices = create_stocks_df(const.tickers_CAC40_dict, "6mo", data_type='prices', remove_nan_by='row')
-    # print(df_prices.shape)
-    # data = yf.Ticker('^FCHI').history(period="6mo")["Close"]
-    # print(data)
-    # cac_prediction_6mo = int(round(make_prediction('6mo')))
-    # #cac_prediction_2y = int(round(make_prediction('2y')))
-    # #cac_prediction_1y = int(round(make_prediction('1y')))
-    #
-    # print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 6 derniers mois.'.format(cac_prediction_6mo))
-    # #print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 12 derniers mois.'.format(cac_prediction_1y))
-    # #print('La prevision du CAC40 pour ce soir est {} se basant sur les données des 24 derniers mois.'.format(cac_prediction_2y))
-
     res = main2()
-    print(res)
+    print('Cac40 forecast : {}'.format(res))
